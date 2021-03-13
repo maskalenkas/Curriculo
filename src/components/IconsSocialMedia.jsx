99f73d4bbd1c0624d19a1499ebbc9dc893c5490e
Arthur <maskalenkasth@gmail.com>
2021-03-13 05:00:43 -0300

    Icons social media implementados

--- src/components/IconsSocialMedia.jsx
+++ src/components/IconsSocialMedia.jsx
@@ -1,5 +1,25 @@
 import '../styles/components/IconsSocialMedia.css';
 
 export default function IconsSocialMedia() {
-  return <div className="iconsSocialMediaContainer">oi</div>;
+  return (
+    <div className="iconsSocialMediaContainer">
+      <ul>
+        <li>
+          <a href="#">
+            <i className="fa fa-facebook-square" aria-hidden="true"></i>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i className="fa fa-github-square" aria-hidden="true"></i>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i className="fa fa-linkedin-square" aria-hidden="true"></i>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
