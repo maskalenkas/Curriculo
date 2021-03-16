fc392fa3a5166cb8142093e6ccb17e3c129d13a0
Arthur <maskalenkasth@gmail.com>
2021-03-16 00:51:37 -0300

    overlay implementado

--- src/pages/Fale.jsx
+++ src/pages/Fale.jsx
@@ -2,18 +2,20 @@ import '../styles/pages/Fale.css';
 
 export default function Fale() {
   return (
-    <div className="faleContainer">
-      <p>
-        O meu nome é Arthur Maskalenkas, tenho 20 anos e atualmente curso o 2º semestre de Análise e Desenvolvimento de
-        sistemas na faculdade FMU.
-      </p>
+    <div className="overlay">
+      <div className="faleContainer">
+        <p>
+          O meu nome é Arthur Maskalenkas, tenho 20 anos e atualmente curso o 2º semestre de Análise e Desenvolvimento
+          de sistemas na faculdade FMU.
+        </p>
 
-      <p>
-        Sou um profissional muito dedicado, empenhado e organizado, e em constante evolução. Estou em busca da minha
-        primeira oportunidade como desenvolvedor Web
-      </p>
+        <p>
+          Sou um profissional muito dedicado, empenhado e organizado, e em constante evolução. Estou em busca da minha
+          primeira oportunidade como desenvolvedor Web
+        </p>
 
-      <p>Aqui vai um resumo das principais tecnologias que eu utilizo</p>
+        <p>Aqui vai um resumo das principais tecnologias que eu utilizo</p>
+      </div>
     </div>
   );
 }
