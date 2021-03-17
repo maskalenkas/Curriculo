c5626ca08b02953f620654dea52af85d166a2e74
Arthur <maskalenkasth@gmail.com>
2021-03-17 02:38:06 -0300

    Tornando o formulario mais responsivo

--- src/pages/Fale.jsx
+++ src/pages/Fale.jsx
@@ -7,12 +7,7 @@ import { ContactForm } from '../components/ContactForm';
 export default function Fale() {
   return (
     <div className="overlay">
-      <div className="faleContainer">
-        <button>
-          <Link to="/">X</Link>
-        </button>
-        <ContactForm />
-      </div>
+      <ContactForm />
     </div>
   );
 }
